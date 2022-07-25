e86b2e23b99b5d0dff3f496db240b9f748d42e85
Kabeer8 <108748863+Kabeer8@users.noreply.github.com>
2022-07-25 09:25:15 -0600

    Update player.js

--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 
-
+let paused = false;
 document.addEventListener("keydown", keydownHandler)
 function keydownHandler(event){
 
@@ -9,8 +9,16 @@ function keydownHandler(event){
     if(event.keyCode === 68){
         player.yVD = 5;
     }
-
-
+    if(event.keyCode === 80){
+        if (paused == false){
+        cancelAnimationFrame(raf);
+        paused = true;
+        }
+        else{
+            animate();
+        paused = false;
+        }
+    }
 }
 document.addEventListener("keyup", keyupHandler)
 function keyupHandler(event){
